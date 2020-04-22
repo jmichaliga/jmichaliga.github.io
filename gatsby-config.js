8326e79c98865df5b04d9831d1d1ec5b1e9f2b9d
Justin Michaliga <justin.michaliga@gmail.com>
2020-04-22 09:37:31 -0400

    rm gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#11626F`,
-        theme_color: `#FA1967`,
-        display: `minimal-ui`,
-        icon: `src/images/jm.svg`, // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
         fonts: [
