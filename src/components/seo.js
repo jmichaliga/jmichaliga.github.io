3dfd591209aaebfbb82979710ef74b62f189d8ed
Justin Michaliga <justin.michaliga@gmail.com>
2020-05-13 13:43:34 -0400

    fix meta data

--- src/components/seo.js
+++ src/components/seo.js
@@ -37,8 +37,8 @@ function SEO({ description, lang, meta, keywords, title }) {
         { href: "/apple-touch-icon.png", rel: "apple-touch-icon", sizes: "180x180" },
         { href: "/favicon-32x32.png", rel: "icon", sizes: "32x32" },
         { href: "/favicon-16x16.png", rel: "icon", sizes: "16x16" },
-        { href: "/site.webmanifest", rel="manifest" },
-        { href="/safari-pinned-tab.svg", rel="mask-icon", color: theme.colors.jmRed },
+        { href: "/site.webmanifest", rel: "manifest" },
+        { href: "/safari-pinned-tab.svg", rel: "mask-icon", color: theme.colors.jmRed },
       ]}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
