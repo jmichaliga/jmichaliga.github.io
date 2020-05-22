8ba53558e5ae8deca0d4d02b06b69762b3fd77b3
Justin Michaliga <justin.michaliga@gmail.com>
2020-05-22 13:04:50 -0400

    Updates

---
+++ component---src-pages-resume-js-51fb940a7780494fc781.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{F2CN:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Resume"}),l.a.createElement("h1",null,"Hi from the Resume"),l.a.createElement("p",null,"Welcome to Resume"),l.a.createElement(o.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-resume-js-51fb940a7780494fc781.js.map
