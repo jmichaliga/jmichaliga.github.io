cfc31424573120ecbb42c615eafcc4188d04f97f
Justin Michaliga <justin.michaliga@gmail.com>
2020-07-23 00:15:54 -0400

    feat: update uses, blog placeholder

---
+++ src/pages/work/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const WorkPage = () => (
+  <Layout>
+    <SEO title="Work: Not found" />
+    <h1>WorkPage</h1>
+  </Layout>
+)
+
+export default WorkPage
