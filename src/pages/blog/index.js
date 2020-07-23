767b16fcafc760857d7b26eba01e38f6d08b9a8c
Justin Michaliga <justin.michaliga@gmail.com>
2020-07-22 23:52:14 -0400

    add new page sections

---
+++ src/pages/blog/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Blog = () => (
+  <Layout>
+    <SEO title="Blog: " />
+    <h1>Blog</h1>
+  </Layout>
+)
+
+export default Blog
