a873595da7645ba10dddd9417f7183d594643ac6
Justin Michaliga <justin.michaliga@gmail.com>
2021-06-26 18:53:34 -0400

    rm + update

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function JMichaliga({ Component, pageProps }) {
 
   useEffect(() => {
     Fathom.load('EOHOGNHG', {
-      includedDomains: ['jmichaliga.com', 'j13a.com', 'jmichaliga.github.io', 'jmichaliga.vercel.app'],
+      includedDomains: ['jmichaliga.com', 'j13a.com', 'jmichaliga.vercel.app'],
     })
 
     function onRouteChangeComplete() {
